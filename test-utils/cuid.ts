d74148703a608c70856b9b008f0f321b2b716129
amirhossein-moloki <amirh.moloki@gmail.com>
2026-01-03 20:13:06 +0330

    Start test database before running Jest

---
+++ test-utils/cuid.ts
@@ -0,0 +1,13 @@
+const alphabet = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+const randomChar = () => alphabet[Math.floor(Math.random() * alphabet.length)];
+
+const cuid = () => {
+  let id = "c";
+  for (let i = 0; i < 24; i += 1) {
+    id += randomChar();
+  }
+  return id;
+};
+
+export default cuid;
